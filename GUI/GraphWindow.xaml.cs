fb615991a2ec98b20e76a762e186f234b5bdd6cb
Marc Kohaupt <debuglevel@gmail.com>
2012-11-27 16:06:03 +0100

    added references for Graphviz4net

---
+++ GUI/GraphWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Book2Chart.Parser;
+
+namespace Book2Chart.GUI
+{
+    /// <summary>
+    /// Interaktionslogik für GraphWindow.xaml
+    /// </summary>
+    public partial class GraphWindow : Window
+    {
+        public Book Book { get; private set; }
+
+        public GraphWindow()
+        {
+            InitializeComponent();
+        }
+
+        public GraphWindow(Book book) : this()
+        {
+            this.Book = book;
+            this.DataContext = book;
+        }
+    }
+}
